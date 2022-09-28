188d5ba47b54fc9abb3a3072d440fe77668746be
djjohns <djjohns@users.noreply.github.com>
2022-09-28 10:54:27 -0500

    tut 17.

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "Home", body: "lasuhgdoiauyg", author: "Bob", id: 1 },
-    { title: "Fun w/ react!", body: "asdfiug", author: "David", id: 2 },
-    { title: "Need a backend?", body: "asdfhjer", author: "David", id: 3 },
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
+      {blogs && <BlogList blogs={blogs} title="All blogs" />}
     </div>
   );
 };
