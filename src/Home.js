0ef07b87689a93e9a001bae1a8e3c0f43db5864f
djjohns <djjohns@users.noreply.github.com>
2022-09-28 10:11:01 -0500

    Tut 13.

--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,19 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    { title: "Home", body: "lasuhgdoiauyg", author: "David", id: 1 },
+    { title: "Home", body: "lasuhgdoiauyg", author: "Bob", id: 1 },
     { title: "Fun w/ react!", body: "asdfiug", author: "David", id: 2 },
     { title: "Need a backend?", body: "asdfhjer", author: "David", id: 3 },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs" />
+      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
     </div>
   );
 };
